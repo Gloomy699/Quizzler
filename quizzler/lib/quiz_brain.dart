8455a3993c183e4ad8aa4b564558753bffbabe38
Yaroslav Vasylenko <gloomy.699@gmail.com>
2021-09-22 13:18:12 +0300

    upd

--- quizzler/lib/quiz_brain.dart
+++ quizzler/lib/quiz_brain.dart
@@ -2,48 +2,62 @@ import 'question.dart';
 
 class QuizBrain {
   int _numberOfQuestion = 0;
+
   final _questionBank = <Question>[
     Question(
-        questionText: 'You can lead a cow down stairs but not up stairs.',
-        questionAnswer: false),
+      questionText: 'You can lead a cow down stairs but not up stairs.',
+      questionAnswer: false,
+    ),
     Question(
-        questionText:
-            'Approximately one quarter of human bones are in the feet.',
-        questionAnswer: true),
-    Question(questionText: 'A slug\'s blood is green.', questionAnswer: true),
+      questionText: 'Approximately one quarter of human bones are in the feet.',
+      questionAnswer: true,
+    ),
     Question(
-        questionText: 'Buzz Aldrin\'s mother\'s maiden name was \"Moon\".',
-        questionAnswer: true),
+      questionText: 'A slug\'s blood is green.',
+      questionAnswer: true,
+    ),
     Question(
-        questionText: 'It is illegal to pee in the Ocean in Portugal.',
-        questionAnswer: true),
+      questionText: 'Buzz Aldrin\'s mother\'s maiden name was \"Moon\".',
+      questionAnswer: true,
+    ),
     Question(
-        questionText:
-            'No piece of square dry paper can be folded in half more than 7 times.',
-        questionAnswer: false),
+      questionText: 'It is illegal to pee in the Ocean in Portugal.',
+      questionAnswer: true,
+    ),
     Question(
-        questionText:
-            'In London, UK, if you happen to die in the House of Parliament, you are technically entitled to a state funeral, because the building is considered too sacred a place.',
-        questionAnswer: true),
+      questionText:
+          'No piece of square dry paper can be folded in half more than 7 times.',
+      questionAnswer: false,
+    ),
     Question(
-        questionText:
-            'The loudest sound produced by any animal is 188 decibels. That animal is the African Elephant.',
-        questionAnswer: false),
+      questionText:
+          'In London, UK, if you happen to die in the House of Parliament, you are technically entitled to a state funeral, because the building is considered too sacred a place.',
+      questionAnswer: true,
+    ),
     Question(
-        questionText:
-            'The total surface area of two human lungs is approximately 70 square metres.',
-        questionAnswer: true),
+      questionText:
+          'The loudest sound produced by any animal is 188 decibels. That animal is the African Elephant.',
+      questionAnswer: false,
+    ),
     Question(
-        questionText: 'Google was originally called \"Backrub\".',
-        questionAnswer: true),
+      questionText:
+          'The total surface area of two human lungs is approximately 70 square metres.',
+      questionAnswer: true,
+    ),
     Question(
-        questionText:
-            'Chocolate affects a dog\'s heart and nervous system; a few ounces are enough to kill a small dog.',
-        questionAnswer: true),
+      questionText: 'Google was originally called \"Backrub\".',
+      questionAnswer: true,
+    ),
     Question(
-        questionText:
-            'In West Virginia, USA, if you accidentally hit an animal with your car, you are free to take it home to eat.',
-        questionAnswer: true),
+      questionText:
+          'Chocolate affects a dog\'s heart and nervous system; a few ounces are enough to kill a small dog.',
+      questionAnswer: true,
+    ),
+    Question(
+      questionText:
+          'In West Virginia, USA, if you accidentally hit an animal with your car, you are free to take it home to eat.',
+      questionAnswer: true,
+    ),
   ];
 
   void nextQuestion() {
---
+++ quizzler/lib/quiz_screen.dart
@@ -0,0 +1,119 @@
+import 'package:flutter/material.dart';
+import 'package:quizzler/main.dart';
+import 'package:rflutter_alert/rflutter_alert.dart';
+
+class QuizPage extends StatefulWidget {
+  @override
+  _QuizPageState createState() => _QuizPageState();
+}
+
+class _QuizPageState extends State<QuizPage> {
+  void _changeQuestion() => quizBrain.nextQuestion();
+
+  void _addCheckMarkTrue() => _scoreKeeper.add(
+        Icon(
+          Icons.check,
+          color: Colors.green,
+        ),
+      );
+
+  void _addCheckMarkFalse() => _scoreKeeper.add(
+        Icon(
+          Icons.close,
+          color: Colors.red,
+        ),
+      );
+
+  var _scoreKeeper = <Widget>[];
+
+  void _checkAnswer(bool userPicktAnswer) {
+    bool correctAnswer = quizBrain.getQuestionAnswer();
+
+    setState(() {
+      if (quizBrain.isFinished() == true) {
+        Alert(
+          context: context,
+          title: 'Finished!',
+          desc: 'You\'ve reached the end of the quiz',
+        ).show();
+        quizBrain.reset();
+        _scoreKeeper = [];
+      } else {
+        if (userPicktAnswer == correctAnswer) {
+          _addCheckMarkTrue();
+        } else {
+          _addCheckMarkFalse();
+        }
+      }
+
+      _changeQuestion();
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      children: [
+        Expanded(
+          flex: 5,
+          child: Padding(
+            padding: EdgeInsets.all(10.0),
+            child: Center(
+              child: Text(
+                quizBrain.getQuestionText(),
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  fontSize: 25.0,
+                  color: Colors.white,
+                ),
+              ),
+            ),
+          ),
+        ),
+        Expanded(
+          child: Padding(
+            padding: EdgeInsets.all(15.0),
+            child: TextButton(
+              style: TextButton.styleFrom(
+                backgroundColor: Colors.green,
+              ),
+              child: Text(
+                'True',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 20.0,
+                ),
+              ),
+              onPressed: () {
+                _checkAnswer(true);
+              },
+            ),
+          ),
+        ),
+        Expanded(
+          child: Padding(
+            padding: EdgeInsets.all(15.0),
+            child: TextButton(
+              style: TextButton.styleFrom(
+                backgroundColor: Colors.red,
+              ),
+              child: Text(
+                'False',
+                style: TextStyle(
+                  fontSize: 20.0,
+                  color: Colors.white,
+                ),
+              ),
+              onPressed: () {
+                _checkAnswer(false);
+              },
+            ),
+          ),
+        ),
+        Wrap(children: _scoreKeeper)
+      ],
+    );
+  }
+}
